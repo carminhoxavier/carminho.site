9c8069bc2386caaf7acad86168259282754728d1
carminhoxavier <xavier.carminho@gmail.com>
2024-04-09 18:21:56 +0100

    first commit

---
+++ email/contact-form-email.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Html,
+  Body,
+  Head,
+  Heading,
+  Hr,
+  Container,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import { Tailwind } from "@react-email/tailwind";
+
+type ContactFormEmailProps = {
+  message: string;
+  senderEmail: string;
+};
+
+export default function ContactFormEmail({
+  message,
+  senderEmail,
+}: ContactFormEmailProps) {
+  return (
+    <Html>
+      <Head />
+      <Preview>Nova mensagem do Site</Preview>
+      <Tailwind>
+        <Body className="bg-gray-100 text-black">
+          <Container>
+            <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
+              <Heading className="leading-tight">
+                Recebeste uma nona mensagem de um ContactFormEmail
+              </Heading>
+              <Text>{message}</Text>
+              <Hr />
+              <Text>O email do remetente é: {senderEmail}</Text>
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
